007c659f76d39ec16184094cc83476287ff590bb
JIE CHEN <jche0193@uni.sydney.edu.au>
2025-03-25 02:24:34 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // 使用静态导出模式 (GitHub Pages 需要)
+    distDir: 'out',    // 默认导出目录，GitHub Pages 只支持 `/out`
+    images: {
+        unoptimized: true // GitHub Pages 可能不支持 Next.js 图像优化功能
+    },
+    basePath: '/Jay-C1.github.io', // 设置为你的 GitHub Pages 仓库名
+    assetPrefix: '/Jay-C1.github.io/' // 静态资源前缀
+};
 
 module.exports = nextConfig
